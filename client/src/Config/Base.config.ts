2598c95dc0469db6be5644d1cdf6ee32b48efc6f
searchme86 <searchme86@gmail.com>
2022-12-10 01:36:15 +0900

    👏🏻client config

---
+++ client/src/Config/Base.config.ts
@@ -0,0 +1,10 @@
+import { firebaseConfig } from './Firebase.config';
+
+const config = {
+  firebase: firebaseConfig,
+  server: {
+    url: 'http://localhost:1337',
+  },
+};
+
+export default config;
--- client/src/Config/Firebase.config.ts
+++ client/src/Config/Firebase.config.ts
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
@@ -16,6 +16,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
